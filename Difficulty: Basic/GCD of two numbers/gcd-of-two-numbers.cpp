68a0b09f107577f5e6d1d5f3ef1965d8412bfdcf
Debojyoti <debojyoti.iiitbh@gmail.com>
2025-07-16 14:32:02 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/GCD of two numbers/gcd-of-two-numbers.cpp
+++ Difficulty: Basic/GCD of two numbers/gcd-of-two-numbers.cpp
@@ -2,16 +2,10 @@ class Solution {
   public:
     int gcd(int a, int b) {
         // code here
-        int n1 = a;
-        int n2 = b;
-        a = max(n1,n2);
-        b = min(n1,n2);
-        
         if(b==0){
             return a;
         }
-        else{
-            return gcd(a%b,b);
-        }
+        gcd(b,a%b);
+        
     }
 };
