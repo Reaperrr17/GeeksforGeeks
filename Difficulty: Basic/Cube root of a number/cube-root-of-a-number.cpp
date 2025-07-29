381d474d37612dd5659d9d1ddd67c73aea0ef6f2
Debojyoti <debojyoti.iiitbh@gmail.com>
2025-07-29 13:20:47 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Cube root of a number/cube-root-of-a-number.cpp
+++ Difficulty: Basic/Cube root of a number/cube-root-of-a-number.cpp
@@ -1,27 +1,26 @@
 class Solution {
   public:
-    int cubeRoot(int n) {
+    int cubeRoot(int x) {
         // code here
-        int st=0;
-        int end=n;
+         int st=0;
+        int end = x;
         int ans;
-        
+
         while(st<=end){
-            long long mid = st + (end-st)/2;
-            
-            if(mid*mid*mid==n){
-                ans = mid;
-                break;
+            long long mid = (st+end)/2;
+
+            if(mid*mid*mid == x){
+                return mid;
             }
-            else if(mid*mid*mid<n){
+            else if(mid*mid*mid<x){
                 ans = mid;
                 st = mid+1;
             }
             else{
                 end = mid-1;
             }
-            
         }
+
         return ans;
     }
 };
