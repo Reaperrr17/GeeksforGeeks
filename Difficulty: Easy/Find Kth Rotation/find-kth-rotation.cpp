d44837a3523fb0fce6a43e3523319c89ed1c8a4a
Debojyoti <debojyoti.iiitbh@gmail.com>
2025-08-17 10:59:44 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Find Kth Rotation/find-kth-rotation.cpp
+++ Difficulty: Easy/Find Kth Rotation/find-kth-rotation.cpp
@@ -1,34 +1,32 @@
 class Solution {
   public:
-    int findKRotation(vector<int> &arr) {
+    int findKRotation(vector<int> &nums) {
         // Code Here
-        int st = 0;
-        int end = arr.size()-1;
-        int N = arr.size();
-        
-        if(arr[st]<=arr[end]){
-            return st;
-        }
-        else{
-            
+        int N = nums.size();
+        if(N==1){
+            return 0;
+        }else if(nums[0]<nums[(N-1)%N] && nums[0]<nums[1]){
+            return 0;
+        }else if(nums[N-1]<nums[N-2] && nums[N-1]<nums[0]){
+            return N-1;
+        }else{
+            int ans;
+            int st = 1;
+            int end = N-2;
+
             while(st<=end){
-                int mid = end - (end-st)/2;
-                
-                if(arr[mid]<=arr[(mid-1+N)%N] && arr[mid]<=arr[(mid+1)%N]){
-                    return mid;
-                }
-                else if(arr[st]>=arr[end] && arr[mid]<=arr[st]){
-                    end = mid-1;
-                }
-                else if(arr[st]<=arr[end] && arr[mid]>=arr[st]){
-                    end = mid-1;
-                }
-                else{
+
+                int mid = st + (end-st)/2;
+                if(nums[mid]<nums[mid-1] && nums[mid]<nums[mid+1]){
+                    ans = mid;
+                    break;
+                }else if(nums[mid]>=nums[0]){
                     st = mid+1;
+                }else{
+                    end = mid-1;
                 }
             }
-            
-            return 0;
+            return ans;
         }
     }
 };
