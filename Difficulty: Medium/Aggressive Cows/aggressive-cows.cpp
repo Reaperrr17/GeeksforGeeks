5781893f39eddbdb2c8a2a9eec81f000584efb6a
Debojyoti <debojyoti.iiitbh@gmail.com>
2025-08-21 22:29:40 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Aggressive Cows/aggressive-cows.cpp
+++ Difficulty: Medium/Aggressive Cows/aggressive-cows.cpp
@@ -1,32 +1,28 @@
 class Solution {
   public:
-    bool ispossible(int maximum_mindistance, vector <int> &stalls,int k){
-        int numberofcowsplaced = 1;
-        int currstall = stalls[0];
+    bool ispossible(int mindist,vector <int> &stalls,int k){
+        int lastpos = stalls[0];
+        int count = 1;
         for(int i=1;i<stalls.size();i++){
-            if(stalls[i]-currstall>=maximum_mindistance){
-                numberofcowsplaced++;
-                currstall = stalls[i];
+            if(stalls[i]-lastpos >= mindist){
+                count++;
+                lastpos = stalls[i];
+                if(count>=k){
+                    return true;
+                }
             }
         }
-        if(numberofcowsplaced<k){
-            return false;
-        }else{
-            return true;
-        }
-        
+        return false;
+       
     }
     int aggressiveCows(vector<int> &stalls, int k) {
         // code here
-        
-        
         sort(stalls.begin(),stalls.end());
         int st = 1;
-        int end = stalls[stalls.size()-1]-stalls[0];
+        int end = stalls[stalls.size()-1] - stalls[0];
         int ans;
         
         while(st<=end){
-            
             int mid = st + (end-st)/2;
             
             if(ispossible(mid,stalls,k)){
